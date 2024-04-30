8550031422af81cdf6c22750911f6416a9499f2c
jerrymusaga <musagatechnology@gmail.com>
2024-04-30 18:24:13 +0100

    generated a key pair from solana/web3.js, stored secret key in env and got the public key securedly

--- generate-keypair/keypair.ts
+++ generate-keypair/keypair.ts
@@ -1,7 +1,10 @@
 import { Keypair } from "@solana/web3.js";
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
-const keypair = Keypair.generate();
+// const keypair = Keypair.generate();
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
 // console.log("Keypair generated", keypair);
 console.log("Public key:", keypair.publicKey.toBase58());
-console.log("Secret key:", keypair.secretKey);
